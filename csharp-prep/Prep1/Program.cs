45ece4a735193ed544a18d5ca1e97a6b91432bd1
Hilvin Kumar <hilvinkv@byui.edu>
2025-01-11 11:40:12 -0700

    did the prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is yout first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is yout last name? ");
+        string last = Console.ReadLine();
+    
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
